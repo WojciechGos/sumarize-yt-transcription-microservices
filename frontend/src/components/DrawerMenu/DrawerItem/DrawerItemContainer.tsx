7f5ef15852cad0cdbb52377d5d2340a7c066f249
Wojtek <wojtekgg93@gmail.com>
2024-04-21 14:58:36 +0200

    add: fetching histories

--- frontend/src/components/DrawerMenu/DrawerItem/DrawerItemContainer.tsx
+++ frontend/src/components/DrawerMenu/DrawerItem/DrawerItemContainer.tsx
@@ -1,28 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import DrawerItemWrapper from './DrawerItemWrapper'
-import { DrawerItemType } from './DrawerItem'
-import axiosInstance from '../../../service/axiosConfig'
+import React, { useEffect, useState, FC } from "react";
+import DrawerItemWrapper from "./DrawerItemWrapper";
+import { DrawerItemType } from "./DrawerItem";
+import axiosInstance from "../../../service/axiosConfig";
+import { History } from "../../../interfaces/History";
 
-
-const DrawerItemContainer = () => {
-  const [items, setItems] = useState<DrawerItemType[]>([
-    { title: "Item 1 mm mmmmmmmmm mmmmmmm mmmmmm mmmmmmmmm" },
-    { title: "Item 2" },
-    { title: "Item 3" },
-  ])
-  
-  useEffect(() => {
-    axiosInstance.get('/api/v1/histories').then((response) => {
-      const data = response.data
-      console.log(data)
-      // setItems(data)
-    })
-  },[])
-
-
-  return (
-    <DrawerItemWrapper items={items}/>
-  )
+interface IDrawerItemContainerProps {
+  toggleDrawer: () => void;
 }
 
-export default DrawerItemContainer
+const DrawerItemContainer: FC<IDrawerItemContainerProps> = ({toggleDrawer}) => {
+  const [items, setItems] = useState<DrawerItemType[]>([]);
+
+  const getHistories = async () => {
+    try {
+      const response = await axiosInstance.get("/api/v1/histories");
+      const data: History[] = response.data;
+      const items: DrawerItemType[] = [];
+      data.forEach((history) => {
+        items.push({ id: history.id, title: history.date });
+      });
+      setItems(items);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getHistories();
+  }, []);
+
+  return <DrawerItemWrapper items={items} toggleDrawer={toggleDrawer} />;
+};
+
+export default DrawerItemContainer;
--- frontend/src/components/SummarizedContent/SummarizedContentContainer.tsx
+++ frontend/src/components/SummarizedContent/SummarizedContentContainer.tsx
@@ -1,8 +1,35 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import SummarizedContent from "./SummarizedContent"
+import axiosInstance from "../../service/axiosConfig"
+import { useParams } from "react-router-dom";
+import { History } from "../../interfaces/History";
+import { useNavigate } from "react-router-dom";
 
 const SummarizedContentContainer = () => {
-  return <SummarizedContent />
+  const navigate = useNavigate()
+  const [data, setData] = useState<History>()
+
+  const { id } = useParams<string>();
+
+  const getSummarizedContent = async () => {
+    try {
+      const response = await axiosInstance.get(`/api/v1/histories/${id}`)  
+      setData(response.data)
+      if(response.status === 401){
+        localStorage.removeItem("token")
+        navigate("/sign-in")
+      }
+    }catch (error) {
+      console.log(error)
+    }
+  
+  }
+
+  useEffect(() => {
+    getSummarizedContent()
+  }, [id])
+
+  return <SummarizedContent data={data} />
 }
 
 export default SummarizedContentContainer
