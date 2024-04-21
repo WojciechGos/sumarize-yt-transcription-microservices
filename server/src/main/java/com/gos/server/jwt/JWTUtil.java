e8a41a04897d3089d66cbad7a2d8257950b56108
Wojtek <wojtekgg93@gmail.com>
2024-04-21 21:28:04 +0200

    change: dockerfile and jwt utils

--- server/src/main/java/com/gos/server/jwt/JWTUtil.java
+++ server/src/main/java/com/gos/server/jwt/JWTUtil.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
@@ -16,8 +17,8 @@ import java.util.Map;
 @Service
 public class JWTUtil {
 
-    private static final String SECRET_KEY =
-            "foobar_123456789_foobar_123456789_foobar_123456789_foobar_123456789";
+    @Value("${jwt.secret-key}")
+    private String SECRET_KEY;
 
 
     public String issueToken(String subject) {
